0d7cda736d82dce19473fc6fdd324059d2cb79e2
Hilary Titus Naor <47435436+hilarytn@users.noreply.github.com>
2025-03-14 18:25:30 +0000

    Db name

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
-            dbName: 'bookcycle'
+            dbName: 'PhantomHealth'
         });
         console.log(`MongoDB connected: ${conn.connection.host}`.green.underline);
     } catch (error) {
