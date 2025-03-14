e1aa2b5a64e00f2d7b34e2fd118cd0f204d62d33
Hilary Titus Naor <47435436+hilarytn@users.noreply.github.com>
2025-03-14 19:03:50 +0000

    Update PORT colour

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=> {
-    console.log(`Server running on PORT ${PORT}`);
+    console.log(`Server running on PORT ${PORT}`.underline.yellow)
 })
