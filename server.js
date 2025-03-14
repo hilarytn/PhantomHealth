5d368115be99ab4ac6c90e3b37ab6eff2919d7a5
Hilary Titus Naor <47435436+hilarytn@users.noreply.github.com>
2025-03-14 18:59:05 +0000

    set up server

--- server.js
+++ server.js
@@ -7,3 +7,10 @@ import connectDB from './config/db.js';
 dotenv.config()
 
 connectDB()
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=> {
+    console.log(`Server running on PORT ${PORT}`);
+})
