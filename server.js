e14b90811279002ac0e2876cb929a9357b7c0b81
Hilary Titus Naor <47435436+hilarytn@users.noreply.github.com>
2025-03-15 08:38:54 +0000

    Set user routing

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import userRoutes from './routes/userRoutes.js'
 
 import connectDB from './config/db.js';
 
@@ -10,6 +11,9 @@ connectDB()
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Routes
+app.use('/api/users', userRoutes);
 
 app.listen(PORT, ()=> {
     console.log(`Server running on PORT ${PORT}`.underline.yellow)
